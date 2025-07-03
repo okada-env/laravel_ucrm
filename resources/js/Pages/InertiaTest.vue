bdf78975b674260cd8fd9a02986703c8b971244e
okada_env <k.okada@env-value.co.jp>
2025-07-04 08:26:02 +0900

    ステップ18

---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+});
+</script>
+
+<template>
+Inertiaテストです。<br>
+<a href="/">aタグ経由です</a><br>
+<Link href="/">Linkタグ経由です</Link>
+
+</template>
