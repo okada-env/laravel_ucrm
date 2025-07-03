819dcb22847218c1138385efb58c10538bc979c8
okada_env <k.okada@env-value.co.jp>
2025-07-04 08:39:55 +0900

    ステップ19

--- resources/js/Pages/InertiaTest.vue
+++ resources/js/Pages/InertiaTest.vue
@@ -12,6 +12,6 @@ defineProps({
 <template>
 Inertiaテストです。<br>
 <a href="/">aタグ経由です</a><br>
-<Link href="/">Linkタグ経由です</Link>
-
+<Link href="/">Linkタグ経由です</Link><br>
+<Link :href="route('inertia.index')">名前付きルートの確認です</Link>
 </template>
