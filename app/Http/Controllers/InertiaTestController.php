44b931e7239159f83dddc1d86cabc4b031e5ae09
okada_env <k.okada@env-value.co.jp>
2025-07-05 09:04:47 +0900

    ステップ29

--- app/Http/Controllers/InertiaTestController.php
+++ app/Http/Controllers/InertiaTestController.php
@@ -10,7 +10,9 @@ class InertiaTestController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Inertia/index');
+        return Inertia::render('Inertia/index',[
+            'blogs' => InertiaTest::all(),
+        ]);
     }
 
     public function create()
